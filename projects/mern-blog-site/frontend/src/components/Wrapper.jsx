8ea2cf2df33525f75419874d308858cfcf224ae5
phelanwhite <phelanwhite.business@gmail.com>
2024-06-24 07:46:56 +0700

    upload

--- projects/mern-blog-site/frontend/src/components/Wrapper.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Wrapper = ({ children }) => {
-  return <div className="mx-auto max-w-[1432px] w-full px-4">{children}</div>;
-};
-
-export default Wrapper;
--- projects/mern-demo-config-features/backend/configs/multer.config.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage({});
-
-const multerConfig = multer({
-  storage: storage,
-});
-
-export default multerConfig;
